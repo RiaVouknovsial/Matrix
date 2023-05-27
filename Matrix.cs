17aa8c087b194c5a3ccc0acbba80d56cff08c806
RiaVouKnovsial <volovik.raisa.kam@gmail.com>
2023-05-27 19:50:17 +0300

    Добавьте файлы проекта.

---
+++ Matrix.cs
@@ -0,0 +1,20 @@
+﻿public class Matrix
+{
+    private int[,] nums;
+
+    public int Rows { get; set; }
+    public int Cols { get; set; }
+
+    public Matrix(int rows, int cols)
+    {
+        Rows = rows;
+        Cols = cols;
+        nums = new int[rows, cols];
+    }
+
+    public int this[int r, int c]
+    {
+        get { return nums[r, c]; }
+        set { nums[r, c] = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 2. Реализуйте класс "Matrix", представляющий двумерную матрицу. 
+//Реализуйте индексатор, который позволяет получать и задавать элементы матрицы по индексу 
+//строки и столбца. Создайте объект типа "Matrix" и заполните его элементами. 
+//Затем выведите некоторые элементы матрицы, используя индексатор.
+
+using System;
+
+Console.WriteLine("Матрица");
+
+Matrix matrix = new Matrix(2, 3);
+matrix[0, 0] = 13;
+matrix[0, 1] = 64;
+matrix[0, 2] = 75;
+matrix[1, 0] = 23;
+matrix[1, 1] = 3;
+matrix[1, 2] = 56;
+
+for (int i = 0; i < matrix.Rows; i++)
+{
+    for (int j = 0; j < matrix.Cols; j++)
+    {
+
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("Некоторые элементы матрицы:");
+Console.WriteLine(matrix[0, 0]);
+Console.WriteLine(matrix[1, 0]);
+Console.WriteLine(matrix[1, 2]);
+
+
